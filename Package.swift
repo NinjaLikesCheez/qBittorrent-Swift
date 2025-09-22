413e2c0aa7960991bec817e3f2121f43f58ce4a0
NinjaLikesCheez <NinjaLikesCheez@users.noreply.github.com>
2025-09-22 08:35:45 +0200

    Use stable version of swift-api-client

--- Package.swift
+++ Package.swift
@@ -3,27 +3,27 @@
 import PackageDescription
 
 let package = Package(
-	name: "QBittorrent",
-	platforms: [
-		.iOS(.v18),
-		.tvOS(.v18),
-		.macOS(.v15)
-	],
-	products: [
-		.library(name: "QBittorrent", targets: ["QBittorrent"])
-	],
-	dependencies: [
-		.package(url: "https://github.com/NinjaLikesCheez/swift-api-client", branch: "main"),
-		.package(url: "https://github.com/apple/swift-log", from: "1.0.0")
-	],
-	targets: [
-		.target(
-			name: "QBittorrent",
-			dependencies: [
-				.product(name: "APIClient", package: "swift-api-client"),
-				.product(name: "Logging", package: "swift-log")
-			]
-		),
-		.testTarget(name: "QBittorrentIntegrationTests", dependencies: [.target(name: "QBittorrent")])
-	]
+  name: "QBittorrent",
+  platforms: [
+    .iOS(.v18),
+    .tvOS(.v18),
+    .macOS(.v15),
+  ],
+  products: [
+    .library(name: "QBittorrent", targets: ["QBittorrent"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/NinjaLikesCheez/swift-api-client", from: "0.0.1"),
+    .package(url: "https://github.com/apple/swift-log", from: "1.0.0"),
+  ],
+  targets: [
+    .target(
+      name: "QBittorrent",
+      dependencies: [
+        .product(name: "APIClient", package: "swift-api-client"),
+        .product(name: "Logging", package: "swift-log"),
+      ]
+    ),
+    .testTarget(name: "QBittorrentIntegrationTests", dependencies: [.target(name: "QBittorrent")]),
+  ]
 )
